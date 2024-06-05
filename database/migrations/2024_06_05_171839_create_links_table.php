cd01ce876384aa366c2a0f600657aba87ade1477
Vinicius Moreira <vinicius.vsm777@gmail.com>
2024-06-05 14:53:05 -0300

    construção de migrações para usuário e links

--- database/migrations/2024_06_05_171839_create_links_table.php
+++ database/migrations/2024_06_05_171839_create_links_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('links', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('profile_image')->nullable();
-            $table->rememberToken();
+            $table->string('link_shorted')->unique();
+            $table->string('link_received');
+            $table->string('name', 100)->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('links');
     }
 };
--- database/migrations/2024_06_05_174056_create_user_links_table.php
+++ database/migrations/2024_06_05_174056_create_user_links_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_links', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('profile_image')->nullable();
-            $table->rememberToken();
+            $table->string('link_id');
+            $table->string('user_id');
+
+            $table->foreign('link_id')->references('id')->on('links');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_links');
     }
 };
